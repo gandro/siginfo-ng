5ce89431f6e57699d49c245f0eaa5bf08624942e
Sebastian Wicki <gandro@gmx.net>
2010-08-10 20:31:47 +0200

    sparc cpuinfo mhz

--- platform/Linux/cpuinfo.lua
+++ platform/Linux/cpuinfo.lua
@@ -23,9 +23,10 @@ if __init__ then
 
     elseif siginfo.ng.arch == "sparc" or siginfo.ng.arch == "sparc64" then
 
+        CPU.MHZ = math.floor(tonumber(cpuinfo["Cpu0ClkTck"], 16) / 1000000)
+
         -- August 1, 2010 by Wynton "crazycusti" Tietjen
         -- dedicated to Nadine
-
         CPU.MODEL = string.gsub(cpuinfo["cpu"], "(%s+)", " ")
         CPU.CORES = cpuinfo["ncpus active"] or 1
 
